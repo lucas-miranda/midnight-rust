f90bd6b9e65557ae6902bbf7d178a5f6e7e169af
Lucas Miranda <miranda.a.lucas@gmail.com>
2022-06-19 21:31:03 -0300

    fix: missing a rendering backend error file

---
+++ src/rendering/backend/render_backend_build_error.rs
@@ -0,0 +1,55 @@
+use std::{
+    error::Error,
+    fmt::{self, Debug, Display, Formatter},
+};
+
+#[derive(Debug)]
+pub enum RenderBackendBuildError {
+    InstanceFailed(wgpu_hal::InstanceError),
+    SurfaceFailed(wgpu_hal::InstanceError),
+    AdapterNotFound,
+    PresentationNotSupported,
+    LogicalDeviceOpenFailed(wgpu_hal::DeviceError),
+    SurfaceConfigureFailed(wgpu_hal::SurfaceError),
+    PipelineLayoutFailed(wgpu_hal::DeviceError),
+    PipelineFailed(wgpu_hal::PipelineError),
+    CommandEncoderFailed(wgpu_hal::DeviceError),
+}
+
+impl Error for RenderBackendBuildError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match &self {
+            Self::InstanceFailed(base_err) => Some(base_err),
+            Self::SurfaceFailed(base_err) => Some(base_err),
+            Self::LogicalDeviceOpenFailed(device_err) => Some(device_err),
+            Self::SurfaceConfigureFailed(surface_err) => Some(surface_err),
+            Self::PipelineLayoutFailed(device_err) => Some(device_err),
+            Self::PipelineFailed(pipeline_err) => Some(pipeline_err),
+            Self::CommandEncoderFailed(device_err) => Some(device_err),
+            _ => None,
+        }
+    }
+}
+
+impl Display for RenderBackendBuildError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match &self {
+            Self::InstanceFailed(base_err)
+                => write!(f, "Failed to instantiate backend: {}", base_err),
+            Self::SurfaceFailed(base_err)
+                => write!(f, "Failed to create surface: {}", base_err),
+            Self::AdapterNotFound => write!(f, "Valid graphic adapter was not found."),
+            Self::PresentationNotSupported => write!(f, "Presentation is not supported."),
+            Self::LogicalDeviceOpenFailed(device_err)
+                => write!(f, "Failed to open logical device: {}", device_err),
+            Self::SurfaceConfigureFailed(surface_err)
+                => write!(f, "Failed to configure surface: {}", surface_err),
+            Self::PipelineLayoutFailed(device_err)
+                => write!(f, "Failed to create pipeline layout: {}", device_err),
+            Self::PipelineFailed(pipeline_err)
+                => write!(f, "Failed to create pipeline: {}", pipeline_err),
+            Self::CommandEncoderFailed(device_err)
+                => write!(f, "Failed to create command encoder: {}", device_err),
+        }
+    }
+}
