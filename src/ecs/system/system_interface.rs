ebca618816ad8ea1bf02ca4301db8b85fd3c6c39
Lucas Miranda <miranda.a.lucas@gmail.com>
2024-03-01 00:22:29 -0300

    feat: remove setup and input from sys interface

--- src/ecs/system/system_interface.rs
+++ src/ecs/system/system_interface.rs
@@ -1,6 +1,5 @@
 use std::any::Any;
 
-use crate::base::ApplicationState;
 use crate::ecs::{
     component::BaseQuery,
     entity::EntitiesIter,
@@ -11,8 +10,6 @@ use super::System;
 
 pub(crate) struct SystemInterface {
     system: Box<dyn Any>,
-    setup_fn: Box<dyn FnMut(&mut Box<dyn Any>)>,
-    input_fn: Box<dyn FnMut(&mut Box<dyn Any>, EntitiesIter<'_>, &mut ApplicationState)>,
     run_fn: Box<dyn FnMut(&mut Box<dyn Any>, EntitiesIter<'_>, &mut FrameState<'_>)>,
 }
 
@@ -20,21 +17,6 @@ impl SystemInterface {
     pub fn wrap<'a, Q: BaseQuery + 'static, S: System<Query<'a> = Q> + 'static>(system: S) -> Self {
         SystemInterface {
             system: Box::new(system),
-            setup_fn: Box::new(|boxed_system| {
-                let sys = boxed_system.downcast_mut::<S>().unwrap();
-                sys.setup();
-            }),
-            input_fn: Box::new(|boxed_system, entities, app_state| {
-                let sys = boxed_system.downcast_mut::<S>().unwrap();
-
-                let mut query = sys.create_query();
-
-                for entity in entities {
-                    query.capture_components(entity.borrow().components());
-                }
-
-                sys.input(query, app_state)
-            }),
             run_fn: Box::new(|boxed_system, entities, state| {
                 let sys = boxed_system.downcast_mut::<S>().unwrap();
                 let mut query = sys.create_query();
@@ -46,14 +28,6 @@ impl SystemInterface {
                 sys.run(query, state);
             }),
         }
-    }
-
-    pub fn setup(&mut self) {
-        (*self.setup_fn)(&mut self.system)
-    }
-
-    pub fn input(&mut self, entities: EntitiesIter<'_>, app_state: &mut ApplicationState) {
-        (*self.input_fn)(&mut self.system, entities, app_state)
     }
 
     pub fn run(&mut self, entities: EntitiesIter<'_>, state: &mut FrameState) {
